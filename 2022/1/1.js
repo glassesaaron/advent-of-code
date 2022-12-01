f2a22042148519635fa21d50ae1bf670bed7bce6
Aaron Montecalvo <aaron@mojotech.com>
2022-12-01 07:53:27 -0500

    feat: 2022, day 1

---
+++ 2022/1/1.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require("path");
+
+const input = fs.readFileSync(path.resolve(__dirname, '1.txt'))
+    .toString()
+    .split('\n');
+
+let elves = [{
+    position: 0,
+    calories: 0,
+}];
+
+(()=>{
+    let elfPosition = 0;
+    for(let i=0;i<input.length;i++){
+        if(input[i] === ''){
+            elfPosition++;
+            elves.push({
+                position: elfPosition,
+                calories: 0,
+            });
+        } else {
+            elves[elves.length-1].calories += parseInt(input[i]);
+        }
+    }
+    elves.sort((a,b)=>{ return a.calories - b.calories; });
+})();
+
+
+const calculateTopCalorieElves = function(count){
+    return elves.slice(elves.length-count,elves.length).reduce((a,c)=>{
+        return a+c.calories;
+    }, 0);
+}
+
+console.log(calculateTopCalorieElves(1));
+
+console.log(calculateTopCalorieElves(3));
