e7de1bf366ef9a851bee78882d3eb788bf0fdc64
glassesaaron <aaron.montecalvo@gmail.com>
2023-12-06 09:39:20 -0500

    feat: 2023, day 6

---
+++ 2023/6/6.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require("path");
+
+const input = fs.readFileSync(path.resolve(__dirname, 'input.txt'))
+    .toString()
+    .split('\n');
+
+let allRaces = [];
+let times = input[0].split(':')[1].trim().split(' ').filter(x=>x!=='');
+let distances = input[1].split(':')[1].trim().split(' ').filter(x=>x!=='');
+
+let combineTime = '';
+let combineDistance = '';
+for(let x in times){
+    allRaces.push({ time: parseInt(times[x]), distance: parseInt(distances[x])});
+    combineTime += times[x];
+    combineDistance += distances[x];
+}
+
+const countWinners = (race) => {
+    let winners = 0;
+    for(let x=0;x<race.time;x++){
+        let amount = x*(race.time-x);
+        if(amount > race.distance){
+            winners++;
+        }
+    }
+    return winners;
+}
+
+const multiplyWinners = (races) => {
+    let sum = 1;
+    for(let race of races){
+        sum *= countWinners(race);
+    }
+    return sum;
+}
+
+console.log(multiplyWinners(allRaces));
+
+console.log(multiplyWinners([{ time: parseInt(combineTime), distance: parseInt(combineDistance) }]));
